827014aaedce299201d6889c90508dfc9cb0e92c
Eslam Mohamed <eslammhamed425@gmail.com>
2023-10-11 21:32:42 +0300

    update

---
+++ models/base_model.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""base_model module"""
+import uuid
+from datetime import datetime
+
+
+class BaseModel:
+    """
+    class BaseModel that defines all
+        common attributes/methods for other classes.
+    """
+    def __str__(self) -> str:
+        """
+        return a human-readable string representation of the object.
+        """
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
+    def __init__(self) -> None:
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def save(self):
+        """
+        updates the public instance
+            attribute updated_at with the current datetime.
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        returns a dictionary containing all keys/values.
+        """
+        obj_dict = self.__dict__.copy()
+        obj_dict["__class__"] = self.__class__.__name__
+        obj_dict["created_at"] = self.created_at.isoformat()
+        obj_dict["updated_at"] = self.updated_at.isoformat()
+        return obj_dict
